40ad7a92522cf6ecb96c624f0a3a965aba1192ca
Meredith Lesly <bitcababy+github@gmail.com>
2012-02-17 13:12:36 -0500

    Added a mess of matchers to match dates

--- lib/pickle/parser/matchers.rb
+++ lib/pickle/parser/matchers.rb
@@ -1,6 +1,12 @@
 module Pickle
   class Parser
     module Matchers
+	
+			@@year_pat = '(?:(?:19|20)\d\d)'
+			@@day_pat = '(?:(?:3[0-1])|(?:2\d)|(?:1\d)|(?:0?[1-9]))'
+			@@abbr_month_names = Date::ABBR_MONTHNAMES.slice(1..12).join('|')
+			@@month_pat = '(?:(?:1[012])|(?:0?[1-9]))'
+	
       def match_ordinal
         '(?:\d+(?:st|nd|rd|th))'
       end
@@ -13,16 +19,50 @@ module Pickle
         '(?:(?:a|an|another|the|that) )'
       end
   
-      def match_quoted
-        '(?:\\\\"|[^\\"]|\\.)*'
-      end
-  
+			def match_year
+				@@year_pat
+			end
+	
+			def match_month_num
+				@@month_pat
+			end
+
+			def match_day
+				@@day_pat
+			end
+			
+			def _match_date1
+				"(?:#{match_year}[-\/]#{match_month_num}[-\/]#{match_day})"
+			end
+			
+			def _match_date2
+				"(?:#{match_day}[-\/]#{match_month_num}[-\/]#{match_year})"
+			end
+			
+			def _match_date3
+				"(?:(?:#{@@abbr_month_names})\s+#{match_day},?\s*#{match_year})"
+			end
+	
+		
+ 			def match_date
+				"(?:#{_match_date1}|#{_match_date2}|#{_match_date3})"
+			end
+
+			def match_quoted
+		     '(?:\\\\"|[^\\"]|\\.)*'
+			end
+
       def match_label
         "(?::? \"#{match_quoted}\")"
       end
 
+			def match_number
+				'\d+'
+			end
+
       def match_value
-        "(?:\"#{match_quoted}\"|nil|true|false|[+-]?[0-9_]+(?:\\.\\d+)?)"
+        "(?:\"#{match_quoted}\"|#{match_date}|nil|true|false|[+-]?[0-9_]+(?:\\.\\d+)?)"
+        # "(?:\"#{match_quoted}\"|nil|true|false|[+-]?[0-9]+(?:\\.\\d+)?)"
       end
 
       def match_field
@@ -79,8 +119,12 @@ module Pickle
         "(?::? \"(#{match_quoted})\")"
       end
   
+      def match_key_and_value_in_field
+        "(?:\\w+: #{match_value})"
+      end
+
       def capture_key_and_value_in_field
-        "(?:(\\w+): #{capture_value})"
+        "(?:(\\w+): (#{match_value}))"
       end
     end
   end
